60315bcf9351443fc8c634676ce55bd314bd9e8e
Fozao85 <fozaojarid85@gmail.com>
2023-06-20 16:18:47 +0100

    backend

---
+++ config/app.config.js
@@ -0,0 +1,9 @@
+require("dotenv").config;
+
+const MONGO_DB_CONFIG = {
+    DB: 'mongodb+srv://fozaojarid85:Ppfsd46FUg8fVjrM@reactbackend.7kyf4cc.mongodb.net/ReactBackendProject?retryWrites=true&w=majority'
+}
+
+module.exports = {
+    MONGO_DB_CONFIG
+}
